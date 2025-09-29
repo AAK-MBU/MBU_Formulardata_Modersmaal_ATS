6b4f9b63cf88de42189d2c5d6b253b7540a5f930
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-09-29 09:50:16 +0200

    rmeoved unused funcs

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -3,7 +3,6 @@
 import json
 
 import urllib.parse
-from urllib.parse import unquote, urlparse
 
 import ast
 
@@ -14,8 +13,6 @@ import requests
 import pandas as pd
 
 from sqlalchemy import create_engine
-
-from mbu_dev_shared_components.msoffice365.sharepoint_api.files import Sharepoint
 
 
 def transform_form_submission(form_serial_number: str, form: dict, mapping: dict) -> dict:
@@ -151,72 +148,3 @@ def get_forms_data(conn_string: str, form_type: str) -> list[dict]:
             print("Invalid JSON in form_data, skipping row.")
 
     return extracted_data
-
-
-def upload_pdf_to_sharepoint(
-    sharepoint_api: Sharepoint,
-    folder_name: str,
-    os2_api_key: str,
-    file_url: str,
-) -> None:
-    """Main function to upload a PDF to Sharepoint."""
-
-    print("Upload PDF to Sharepoint started.")
-
-    existing_pdfs = sharepoint_api.fetch_files_list(folder_name=folder_name)
-
-    if existing_pdfs:
-        existing_pdf_names = {file["Name"] for file in existing_pdfs}
-
-    else:
-        existing_pdf_names = set()
-
-    path = urlparse(file_url).path
-    filename = path.split("/")[-1]
-    final_filename = f"{unquote(filename)}"
-
-    print(file_url)
-    print(final_filename)
-
-    if final_filename in existing_pdf_names:
-        print(f"File {final_filename} already exists in Sharepoint. Skipping download.")
-
-        return
-
-    print("Downloading PDF from OS2Forms API.")
-    try:
-        downloaded_file = download_file_bytes(file_url, os2_api_key)
-
-    except requests.RequestException as error:
-        print(f"Failed to download file: {error}")
-
-    # Upload the file to Sharepoint
-    sharepoint_api.upload_file_from_bytes(
-        binary_content=downloaded_file,
-        file_name=final_filename,
-        folder_name=folder_name,
-    )
-
-
-def download_file_bytes(url: str, os2_api_key: str) -> bytes:
-    """Downloads the content of a file from a specified URL, appending an API key to the URL for authorization.
-    The API key is retrieved from an environment variable 'OS2ApiKey'.
-
-    Parameters:
-    url (str): The URL from which the file will be downloaded.
-    os2_api_key (str): The API-key for OS2Forms api.
-
-    Returns:
-    bytes: The content of the file as a byte stream.
-
-    Raises:
-    requests.RequestException: If the HTTP request fails for any reason.
-    """
-
-    headers = {"Content-Type": "application/json", "api-key": f"{os2_api_key}"}
-
-    response = requests.request(method="GET", url=url, headers=headers, timeout=60)
-
-    response.raise_for_status()
-
-    return response.content
